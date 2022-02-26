82183dd9439c54f7423f45d0b3411f98139298e2
ElderSam <eldersamuel98@gmail.com>
2022-02-26 12:38:38 -0300

    feat: show memory usage every second (using setInterval)

--- pcRamUsage.js
+++ pcRamUsage.js
@@ -1,18 +1,28 @@
-const os = require('os');
+const os = require("os");
 
 // console.log('OS module - keys:');
 // console.log(Object.keys(os));
 
-const { arch, platform, totalmem, freemem } = os;
-const getGBFromBytes = (v) => parseInt(v  / 1024 / 1024);
-const tRam = getGBFromBytes(totalmem());
-const fRam = getGBFromBytes(freemem());
+setInterval(() => {
+	getMemoryInfo();
+}, 1000);
 
-const stats = {
-    OS: platform(),
-    Arch: arch(),
-    TotalRAM: tRam,
-    FreeRAM: fRam,
-}
+const getMemoryInfo = () => {
+	const { arch, platform, totalmem, freemem } = os;
+	const getMBFromBytes = (v) => parseInt(v / 1024 / 1024);
 
-console.log(stats);
+    const tRam = getMBFromBytes(totalmem());
+	const fRam = getMBFromBytes(freemem());
+    const usage = 100 - (fRam / tRam) * 100;
+
+	const stats = {
+		OS: platform(),
+		Arch: arch(),
+		TotalRAM: `${tRam} MB`,
+		FreeRAM: `${fRam} MB`,
+        Usage: `${usage.toFixed(2)} %`
+	};
+
+    console.clear();
+	console.table(stats);
+};
